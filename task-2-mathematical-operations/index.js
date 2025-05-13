5468edc69cd08d0735153e2a88f7d08dfb89d380
Paul Tunda <paultunda35@gmail.com>
2025-05-13 14:49:22 -0400

    added more form validation features to task-2-mathematical-operations/index.js

--- task-2-mathematical-operations/index.js
+++ task-2-mathematical-operations/index.js
@@ -18,6 +18,12 @@ document.getElementById("my_button").onclick = function() {
             return;
         }
 
+        // Check if the user provides valid input
+        if (isNaN(numberOfUnits) || isNaN(pricePerUnit) || isNaN(taxRate)) {
+            document.getElementById("warning_message").innerHTML= "Please enter a valid number.";
+            return;
+        }
+
 
 
         // Clear warning message if input is valid
@@ -30,8 +36,22 @@ document.getElementById("my_button").onclick = function() {
         let price = Number(pricePerUnit);
         let rate = Number(taxRate);
 
+
+        // Check if the user provides a value greater than 0
+        if (quantity <= 0 || price <= 0 || rate <= 0) {
+            document.getElementById("warning_message").innerHTML= "Please enter a greater than 0.";
+            return;
+        }
+
+        // Clear warning message if input is valid
+        document.getElementById("warning_message").innerHTML = "";
+
+
+
         // Format the full name and displaying name initials
         let totalCost = calculateTotalCost(price, quantity, rate);
+
+
         document.getElementById("total_cost").innerHTML = `Your total cost is $${totalCost}`;
 
     }
